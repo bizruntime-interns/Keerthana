cf05c91989826d819eef1aaac376ca70b61ecc9c
KeerthanaJayakumar <47623021+KeerthanaJayakumar@users.noreply.github.com>
2020-02-10 17:56:11 +0530

    Add files via upload

---
+++ 10-2-20/File_Write.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileWrite
+{
+    class Write_File
+    {
+        static void Main(string[] args)
+        {
+            string[] text = new string[] { "Keerthana", "Jayakumar" };
+            
+            using(StreamWriter write=new StreamWriter("F:/Keerthana/10-2-20/write.txt"))
+            {
+                foreach(string texts in text)
+                {
+                    write.WriteLine(texts);
+                }
+            }
+            
+             Console.ReadKey();
+
+                
+            
+        }
+    }
+}
---
+++ 10-2-20/File_directory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace File_directory
+{
+    class Directory
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"F:/bigbasket");
+            FileInfo[] f = dir.GetFiles();
+            foreach(FileInfo file in f)
+            {
+                Console.WriteLine("File name:{0},Size:{1}",file.Name,file.Length);
+            }
+            Console.ReadKey();
+        }
+    }
+}
