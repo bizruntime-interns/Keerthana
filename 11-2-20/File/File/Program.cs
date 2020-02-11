cb3a1c9e828c88ca46afe185f687418b6dd5361a
KeerthanaJayakumar <47623021+KeerthanaJayakumar@users.noreply.github.com>
2020-02-11 17:52:53 +0530

    Add files via upload

---
+++ 11-2-20/File/File/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace File
+{
+    class Disposable
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr=null;
+            try
+            {
+                var path = "F:/hey.txt";
+               sr  = new StreamReader(path);
+                Console.WriteLine(sr.ReadToEnd());
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(UnauthorizedAccessException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                sr?.Dispose();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 11-2-20/Files/Files/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Files
+{
+    class Files_Binary
+    {
+        static void Main(string[] args)
+        {
+            int i = 100;
+            string s = "name";
+            float f = 40.0f;
+            char c = 'a';
+            //creation of the file
+            BinaryWriter bw = new BinaryWriter(new FileStream("data.txt", FileMode.Create));
+            //writing into the file
+            bw.Write(i);
+            bw.Write(s);
+            bw.Write(f);
+            bw.Write(c);
+
+            bw.Close();
+            //reading the data from the file 
+            try
+            {
+                BinaryReader br = new BinaryReader(new FileStream("data.txt", FileMode.Open));
+                i = br.ReadInt32();
+                Console.WriteLine("Integer value:{0}", i);
+                s = br.ReadString();
+                Console.WriteLine("string value:{0}",s);
+                f = br.ReadInt64();
+                Console.WriteLine("float value:{0}",f);
+                c = br.ReadChar();
+                Console.WriteLine("Character value:{0}", c);
+                br.Close();
+            }
+            catch(EndOfStreamException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+            Console.ReadKey();
+            
+        }
+    }
+}
