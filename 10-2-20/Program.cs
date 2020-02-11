204451934be142a9b6cbf17bd281c56974f5415f
KeerthanaJayakumar <47623021+KeerthanaJayakumar@users.noreply.github.com>
2020-02-11 17:51:12 +0530

    Add files via upload

---
+++ 10-2-20/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIOApp
+{
+    class Readfile
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader read=new StreamReader("F:/hey.txt"))
+            {
+                string line;
+                while ((line = read.ReadLine()) != null)
+                    Console.WriteLine(line);
+                Console.ReadKey();
+            }
+        }
+    }
+}
